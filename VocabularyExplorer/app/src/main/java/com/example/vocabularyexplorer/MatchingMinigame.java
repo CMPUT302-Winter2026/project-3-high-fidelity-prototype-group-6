5f6b557b498d6cf334580472c7268a49c6a47c08
zliang12 <jason.liang5129@gmail.com>
2026-04-07 12:04:38 -0600

    started on matching minigame screen

---
+++ VocabularyExplorer/app/src/main/java/com/example/vocabularyexplorer/MatchingMinigame.java
@@ -0,0 +1,24 @@
+package com.example.vocabularyexplorer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MatchingMinigame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.matching_minigame_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.matching_minigame), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
